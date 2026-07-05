6cb4ea7327a4305987eb3e2161a1ced3381f961d
unknown <trap.trick@gmail.com>
2026-07-05 12:16:23 +0900

    フォルダ構成変更

--- scripts/mindar.js
+++ scripts/mindar.js
@@ -10,7 +10,7 @@ class Loader {
     }
     async init(url, first_action_name) {
         const dracoLoader = new DRACOLoader();
-        dracoLoader.setDecoderPath('./draco_decoder/');
+        dracoLoader.setDecoderPath('./scripts/draco_decoder/');
         const gltf = await new Promise((resolve) => {
             const loader = new GLTFLoader();
             loader.setDRACOLoader(dracoLoader);
@@ -59,14 +59,14 @@ const setup = async () => {
     
     // メインキャラクター設置
     avatar = new Loader();
-    await avatar.init('./venetim.glb');
+    await avatar.init('../models/venetim.glb');
     avatar.gltf.scene.rotation.x = Math.PI / 2;
     avatar.gltf.scene.scale.set(0.7, 0.7, 0.7);
     anchor.group.add(avatar.gltf.scene);
 
     // マスク用メッシュ設置
     mask = new Loader();
-    await mask.init('./mask.glb');
+    await mask.init('../models/mask.glb');
     mask.gltf.scene.rotation.x = Math.PI / 2;
     mask.gltf.scene.scale.set(0.7, 0.7, 0.7);
     mask.gltf.scene.traverse((object) => {
--- scripts/viewer.js
+++ scripts/viewer.js
@@ -10,7 +10,7 @@ class Loader {
     }
     async init(url, first_action_name) {
         const dracoLoader = new DRACOLoader();
-        dracoLoader.setDecoderPath('./draco_decoder/');
+        dracoLoader.setDecoderPath('./scripts/draco_decoder/');
         const gltf = await new Promise((resolve) => {
             const loader = new GLTFLoader();
             loader.setDRACOLoader(dracoLoader);
@@ -76,14 +76,14 @@ const setup = async () => {
 
     // メインキャラクター設置
     avatar = new Loader();
-    await avatar.init('./venetim.glb');
+    await avatar.init('../models/venetim.glb');
     avatar.gltf.scene.scale.set(0.7, 0.7, 0.7);
     scene.add(avatar.gltf.scene);　//MindMR側だとアンカーを追加してる部分
     avatar.gltf.scene.rotation.y = Math.PI / 2; //カメラ位置へ向ける
     
     //　以下本来はマスク用だがビューワでは表示する
     mask = new Loader();
-    await mask.init('./mask.glb');
+    await mask.init('../models/mask.glb');
     mask.gltf.scene.scale.set(0.7 ,0.7 ,0.7);
     mask.gltf.scene.rotation.y = Math.PI / 2; //カメラ位置へ向ける
     scene.add(mask.gltf.scene);
